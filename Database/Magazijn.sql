ed0f64b19ea1d43c38da8dbd1f3e8b182ffa81e9
Bo Mengels <12003586@student.pxl.Be>
2023-12-13 19:02:05 +0100

    Update Magazijn.sql

--- Database/Magazijn.sql
+++ Database/Magazijn.sql
@@ -7,37 +7,27 @@ CREATE TABLE IF NOT EXISTS Rek (
     RekID INT PRIMARY KEY,
     RekName VARCHAR(255) NOT NULL ,
     AantalProducten INT DEFAULT 0,
-    MaxAantalProducten INT DEFAULT 10,
+    MaxAantalProducten INT DEFAULT 20,
     IsEmpty BOOLEAN DEFAULT TRUE
 );
-
 
 CREATE TABLE Product (
     ProductID INT PRIMARY KEY,
     ProductNaam VARCHAR(255),
     RekID INT,
     Stock INT DEFAULT 0,
-    MinVoorraad INT DEFAULT 5,
+    MinimaleVoorraad INT DEFAULT 5, -- Minimale voorraadniveau
     IsEmpty BOOLEAN DEFAULT TRUE,
     FOREIGN KEY (RekID) REFERENCES Rek(RekID)
 );
 
-
--- Creëer de database voor orders
-CREATE DATABASE IF NOT EXISTS OrderDatabase;
-USE OrderDatabase;
-
 -- Creëer de tabel voor orders
 CREATE TABLE OrderTabel (
     OrderID INT PRIMARY KEY,
-    RekID INT,
+    UserID INT,
     ProductID INT,
     ProductNaam VARCHAR(255),
-    Hoeveelheid INT,
-    FOREIGN KEY (RekID) REFERENCES Rek(RekID),
+    Quantity INT,
+    TotalAmount INT,
     FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
 );
-
-
-
-    
